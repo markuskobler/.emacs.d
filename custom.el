e60d4c23c2165922760129130ce2d43c382bac60
Markus Kobler <markus@distinctive.co>
2014-11-08 18:17:37 +0000

    Removing cruft

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" default)))
- '(speedbar-show-unknown-files t))
+ '(custom-safe-themes (quote ("9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -3,204 +3,47 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq inhibit-startup-message t)
 
-;; Set path to dependencies
-(setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
+(setq is-mac    (equal system-type 'darwin))
+(setq lisp-path (expand-file-name "lisp" user-emacs-directory))
 
-;; Set up load path
-(add-to-list 'load-path user-emacs-directory)
-(add-to-list 'load-path site-lisp-dir)
+(add-to-list 'load-path lisp-path)
 
-;; Keep emacs Custom-settings in separate file
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
+;; Add external projects to load path
+(dolist (project (directory-files lisp-path t "\\w+"))
+  (when (file-directory-p project)
+	(add-to-list 'load-path project)))
 
 (require 'appearance)
 
-;; Settings for currently logged in user
-(setq user-settings-dir
-      (concat user-emacs-directory "users/" user-login-name))
-(add-to-list 'load-path user-settings-dir)
-
-;; Add external projects to load path
-(dolist (project (directory-files site-lisp-dir t "\\w+"))
-  (when (file-directory-p project)
-    (add-to-list 'load-path project)))
-
-;; Write backup files to own directory
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
-(setq auto-save-file-name-transforms
-      `(("." ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
-
-(require 'saveplace)
-(setq-default save-place t)
-(setq save-place-file (expand-file-name ".places" user-emacs-directory))
-
-;; Are we on a mac?
-(setq is-mac (equal system-type 'darwin))
-
-;; Setup packages
 (require 'setup-package)
 
-;; Install extensions if they're missing
-(defun init--install-packages ()
-  (packages-install
-   '(magit
-     paredit
-     auto-complete
-     move-text
-     gist
-     htmlize
-     visual-regexp
-     flycheck
-     flx
-     flx-ido
-     css-eldoc
-     yasnippet
-     go-mode
-     projectile
-     smartparens
-     ido-vertical-mode
-     ido-at-point
-     guide-key
-     nodejs-repl
-     restclient
-     highlight-escape-sequences
-     whitespace-cleanup-mode
-     elisp-slime-nav
-     git-commit-mode
-     gitconfig-mode
-     gitignore-mode
-     clojure-mode
-     web-mode
-     prodigy
-     cider
-     cider-tracing)))
+(defun init--install-packages ()                                               
+  (packages-install                                                            
+   '(magit                                                                     
+     paredit                                                                   
+     auto-complete                                                             
+     uniquify                                                             
+     visual-regexp)))
 
-(condition-case nil
-    (init--install-packages)
-  (error
-   (package-refresh-contents)
+(condition-case nil                                                            
+    (init--install-packages)                                                   
+  (error                                                                       
+   (package-refresh-contents)                                                  
    (init--install-packages)))
 
-(require 'sane-defaults)
+(eval-after-load 'magit '(require 'setup-magit))
 
-;; Setup environment variables from the user's shell.
+;; (require 'uniquify)
+;;(require 'undo-tree)
+;;(global-undo-tree-mode)
+
+(load (expand-file-name "custom.el" user-emacs-directory))
+
+;;Setup environment variables from the user's shell.
+
 (when is-mac
   (require-package 'exec-path-from-shell)
   (exec-path-from-shell-initialize))
-
-(eval-after-load 'ido '(require 'setup-ido))
-(eval-after-load 'org '(require 'setup-org))
-(eval-after-load 'dired '(require 'setup-dired))
-(eval-after-load 'magit '(require 'setup-magit))
-(eval-after-load 'grep '(require 'setup-rgrep))
-(require 'setup-hippie)
-(require 'setup-yasnippet)
-(require 'setup-perspective)
-(require 'setup-paredit)
-(require 'setup-ffip)
-(require 'setup-html-mode)
-
-;; Font lock dash.el
-(eval-after-load "dash" '(dash-enable-font-lock))
-
-;; Default setup of smartparens
-(require 'smartparens-config)
-(setq sp-autoescape-string-quote nil)
-(--each '(css-mode-hook
-          restclient-mode-hook
-          js-mode-hook
-          java-mode
-          ruby-mode
-          markdown-mode)
-  (add-hook it 'turn-on-smartparens-mode))
-
-;; Language specific setup files
-(eval-after-load 'js2-mode '(require 'setup-js2-mode))
-(eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
-(eval-after-load 'go-mode '(require 'setup-go-mode))
-
-;; Load on demand
-(autoload 'skewer-start "setup-skewer" nil t)
-(autoload 'skewer-demo "setup-skewer" nil t)
-(autoload 'flycheck-mode "setup-flycheck" nil t)
-(autoload 'auto-complete-mode "auto-complete" nil t)
-
-;; sql setup
-(setq auto-mode-alist
-      (append '(("\\.sql$" . sql-mode)
-                ("\\.ddl$" . sql-mode))
-              auto-mode-alist))
-(add-hook 'sql-mode-hook 'sql-highlight-postgres-keywords)
-
-;; Map files to modes
-(require 'mode-mappings)
-
-;; Highlight escape sequences
-(require 'highlight-escape-sequences)
-(hes-mode)
-(put 'font-lock-regexp-grouping-backslash 'face-alias 'font-lock-builtin-face)
-
-;; Visual regexp
-(require 'visual-regexp)
-
-;; Functions (load all files in defuns-dir)
-(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
-(dolist (file (directory-files defuns-dir t "\\w+"))
-  (when (file-regular-p file)
-    (load file)))
-
-(require 'expand-region)
-(require 'multiple-cursors)
-(require 'delsel)
-(require 'jump-char)
-(require 'wgrep)
-(require 'smart-forward)
-(require 'change-inner)
-(require 'tramp)
-(setq tramp-default-method "ssh")
-
-(require 'projectile)
-(projectile-global-mode)
-(setq projectile-enable-caching t)
-
-(require 'git-gutter)
-(global-git-gutter-mode t)
-
-(require 'find-file-in-project)
-(setq ffip-prune-patterns
-      '(".git" "bower_components" "node_modules" "third_party"))
-
-;; Fill column indicator
-(require 'fill-column-indicator)
-(setq fci-rule-color "#111122")
-
-;; Browse kill ring
-(require 'browse-kill-ring)
-(setq browse-kill-ring-quit-action 'save-and-restore)
-
-(require 'ansi-color)
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
 (when is-mac (require 'mac))
 
-(require 'key-bindings)
-
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-
-;; Run at full power please
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-
-(flx-ido-mode 1)
-
-;; Conclude init by setting up specifics for the current user
-(when (file-exists-p user-settings-dir)
-  (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
+(require 'defaults)
