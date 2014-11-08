0b12bb512c0dff975d87cb70f34ed19d95838f07
Markus Kobler <markus@distinctive.co>
2014-11-08 19:24:42 +0000

    Adding go/js support

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
+(setq inhibit-startup-message t)
+
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(setq inhibit-startup-message t)
 
 (setq is-mac    (equal system-type 'darwin))
 (setq lisp-path (expand-file-name "lisp" user-emacs-directory))
@@ -16,38 +17,53 @@
 (require 'appearance)
 
 (require 'setup-package)
-
-(defun init--install-packages ()                                               
-  (packages-install                                                            
+(defun init--install-packages ()
+  (packages-install
    '(magit
      magit-filenotify
      magit-find-file
      git-blame
      git-gutter
-     paredit                                                                   
-     auto-complete                                                             
-     visual-regexp)))
+     paredit
+     auto-complete
+     multiple-cursors
+     visual-regexp
+     go-mode
+     gotest
+     go-autocomplete
+     go-errcheck
+     flymake
+     flymake-go
+     flymake-jshint
+     flymake-json
+     flymake-sass
+     js3-mode
+     flyspell-lazy
+     )))
 
-(condition-case nil                                                            
-    (init--install-packages)                                                   
-  (error                                                                       
-   (package-refresh-contents)                                                  
+(condition-case nil
+    (init--install-packages)
+  (error
+   (package-refresh-contents)
    (init--install-packages)))
 
-;;(eval-after-load 'magit '(require 'setup-magit))
-
 (require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
 
-;;(require 'undo-tree)
-;;(global-undo-tree-mode)
 
-(load (expand-file-name "custom.el" user-emacs-directory))
+(eval-after-load 'magit             '(require 'setup-magit))
+(eval-after-load "multiple-cursors" '(require 'multiple-cursors))
 
-;;Setup environment variables from the user's shell.
+(autoload 'auto-complete-mode "auto-complete" nil t)
+(autoload 'magit-status "magit")
+(autoload 'dired-jump "dired")
+
 
 (when is-mac
   (require-package 'exec-path-from-shell)
   (exec-path-from-shell-initialize))
 (when is-mac (require 'mac))
 
+(load (expand-file-name "custom.el" user-emacs-directory))
 (require 'defaults)
+(require 'key-bindings)
