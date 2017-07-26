f6c7bd609a16793559a01bd726e70e8b678e4081
Markus Kobler <markus@distinctive.co>
2017-07-26 17:18:17 -0400

    Adding reason support

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
   (run-hooks 'after-init-hook))
 
 (progn
-  (dolist (p '("init" "vendor/lsp-rust" "/Users/markus/code/vendor/reason-mode"))
+  (dolist (p '("init" "vendor/lsp-rust" "vendor/reason"))
     (add-to-list 'load-path
                  (expand-file-name p user-emacs-directory))))
 
@@ -414,47 +414,19 @@
   ;;   (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t))
   )
 
-(use-package merlin
-  :ensure t
-  :mode (("\\.ml\\'" . merlin-mode)
-         ("\\.mli\\'" . merlin-mode))
+;;
+;; reason
+;;
+(use-package reason-mode
+  :ensure f
+  :mode (("\\.re\\'" . reason-mode)
+         ("\\.rei\\'" . reason-mode))
   :config
-  (defun chomp-end (str)
-    "Chomp tailing whitespace from STR."
-    (replace-regexp-in-string (rx (* (any " \t\n")) eos)
-                              ""
-                              str))
-
-  (defun shell-cmd (cmd)
-    "Returns the stdout output of a shell command or nil if the command returned
-   an error"
-    (let ((stdoutput (chomp-end
-                      (with-output-to-string
-                        (with-current-buffer
-                            standard-output
-                          (process-file shell-file-name nil
-                                        '(t nil)  nil
-                                        shell-command-switch cmd))))))
-      (when (not (= (length stdoutput) 0))
-        stdoutput)))
-
-  (let* ((refmt-bin (shell-cmd "refmt ----where"))
-         (merlin-bin (shell-cmd "ocamlmerlin ----where"))
-         (merlin-base-dir (when merlin-bin (replace-regexp-in-string "bin/ocamlmerlin$" "" merlin-bin))))
-    ;; Add npm merlin.el to the emacs load path and tell emacs where to find ocamlmerlin
-    (when merlin-bin
-      (add-to-list 'load-path (concat merlin-base-dir "share/emacs/site-lisp/"))
-      (setq merlin-command merlin-bin))
-
-    (when refmt-bin
-      (setq refmt-command refmt-bin)))
-
-  (require 'reason-mode)
-  (require 'merlin)
-  (add-hook 'reason-mode-hook (lambda ()
-                                (add-hook 'before-save-hook 'refmt-before-save)
-                                (merlin-mode)))
-
+  (use-package merlin :ensure t)
+  ;; (add-hook 'reason-mode-hook
+  ;;           (lambda ()
+  ;;             ;; (add-hook 'before-save-hook 'refmt-before-save)
+  ;;             (merlin-mode)))
   (setq merlin-ac-setup t))
 
 ;;
